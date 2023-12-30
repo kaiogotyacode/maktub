1ee40ce51c55d02f7e2cf4f9cc0e3c635f18fd67
Kaio Maciel <kaioreis1153@hotmail.com>
2023-12-30 09:55:50 -0300

    #001 - Added Initial Design [Mobile]

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,7 +1,34 @@
+import { FaAlignJustify } from "react-icons/fa";
+import style from "./NavBar.module.css";
+
 function NavBar() {
   return (
     <>
-      <h1 className="bg-red-600">NavBar</h1>
+      <nav className={style.nav}>
+        <div className={style.menu}>
+          <FaAlignJustify className={style.menuIcon} />
+        </div>
+        <div className={style.logo}>
+          <h1 className="font-bold">NavBar</h1>
+        </div>
+
+        <div className={style.divOptions}>
+          <ul className={style.options}>
+            <li className={style.itemOption}>
+              <a>Agendas</a>
+            </li>
+            <li className={style.itemOption}>
+              <a>Canetas</a>
+            </li>
+            <li className={style.itemOption}>
+              <a>Cadernos</a>
+            </li>
+          </ul>
+          <div className={style.optionsBackDrop}>
+            
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
