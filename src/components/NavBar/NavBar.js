087604fff0903baa0b970c0f5ac8d28b47695b34
Kaio Maciel <kaioreis1153@hotmail.com>
2023-12-30 09:18:47 -0300

    #001 - Installed TailwindCSS

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,9 @@
+function NavBar() {
+  return (
+    <>
+      <h1 className="bg-red-600">NavBar</h1>
+    </>
+  );
+}
+
+export default NavBar;
---
+++ src/pages/home/Home.js
@@ -0,0 +1,7 @@
+import NavBar from '../../components/NavBar/NavBar'
+
+function Home() {
+  return <NavBar />;
+}
+
+export default Home;
