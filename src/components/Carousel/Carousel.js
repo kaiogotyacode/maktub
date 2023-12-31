54f0a991ce0b6c68ae9371efe8548648b07048ee
Kaio Maciel <kaioreis1153@hotmail.com>
2023-12-31 14:55:55 -0300

    #003 - Added Carousel Shell

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,12 @@
+import style from './Carousel.module.css'
+
+function Carousel(){
+    return (
+        <>
+            <div className={style.carouselContainer}>
+                
+            </div>
+        </>
+    )
+}
+export default Carousel
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,13 @@
-import NavBar from '../../components/NavBar/NavBar'
+import Carousel from "../../components/Carousel/Carousel";
+import NavBar from "../../components/NavBar/NavBar";
 
 function Home() {
-  return <NavBar />;
+  return (
+    <>
+      <NavBar />
+      <Carousel/>
+    </>
+  );
 }
 
 export default Home;
