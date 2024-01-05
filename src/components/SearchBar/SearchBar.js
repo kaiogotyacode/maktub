4597b32544438946e65fc5669d81d43188ade3db
Kaio Maciel <kaioreis1153@hotmail.com>
2024-01-05 07:28:40 -0300

    #002 - Added SearchBar Full Design | Mobile | Desktop

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,13 +1,23 @@
 import style from "./SearchBar.module.css";
-
+import { GiMagnifyingGlass } from "react-icons/gi";
+import { CiEraser } from "react-icons/ci";
 function SearchBar() {
   return (
     <>
       <div className={style.searchBarContainer}>
         <div className={style.searchBar}>
           {/* Lupa Logo */}
+          <span className={style.magnifierContainer}>
+            <GiMagnifyingGlass className={style.magnifierIcon}/>
+          </span>
           {/* Campo de Pesquisa */}
+          <div className={style.search}>
+            <input className={style.searchInput} type="text" placeholder="Type here..."/> 
+          </div>
           {/* Separador | Pesquisa */}
+          <span className={style.searchDivisor}> 
+            <CiEraser className={style.eraserIcon}/>
+          </span>
         </div>
       </div>
     </>
